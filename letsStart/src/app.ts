765bcd551d882a1ecdbf015c9c96e67c9fb641c9
DoosicEE <strou73@naver.com>
2022-12-01 00:28:52 +0900

    #Update middleware
    미들웨어 학습 및 에러 처리 학습.

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -3,13 +3,81 @@ import { Cat, CatType } from "./app.models";
 
 const app: express.Express = express();
 
-const port: number = 8000;
-
-app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req.params);
-  res.send({ cats: Cat });
+// * logging middleware
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middleware");
+  next();
 });
 
+//* json middleware
+app.use(express.json());
+
+//* READ 고양이 전체 데이터 조회
+app.get("/cats", (req: express.Request, res: express.Response) => {
+  try {
+    const cats = Cat;
+    // throw new Error("db connect error");
+    res.send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* READ 특정 고양이 데이터 조회
+app.get("/cats/:id", (req: express.Request, res: express.Response) => {
+  try {
+    const params = req.params;
+    const cats = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* CREATE 새로운 고양이 추가 api
+app.post("/cats", (req: express.Request, res: express.Response) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Cat.push(data); // create
+    res.status(200).send({
+      success: true,
+      data: { data },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+// * 404 middlewart
+app.use((req, res, next) => {
+  console.log("this is error middlewart");
+  res.send({ error: "404 not found error" });
+});
+
+const port: number = 8000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}/`);
 });
