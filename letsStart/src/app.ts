bc2c1bc1acfed2e6365949aa19cfb22464633c73
DoosicEE <strou73@naver.com>
2022-12-02 23:21:56 +0900

    #Update Router 모듈화
    라우터를 모듈화하였다. crud보다는 express로 프로젝트를 진행할때의 구조를 살펴보는 학습이다.

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { Cat, CatType } from "./app.models";
+import catsRouter from "./cats/cats.route";
 
 const app: express.Express = express();
 
@@ -13,63 +13,7 @@ app.use((req, res, next) => {
 //* json middleware
 app.use(express.json());
 
-//* READ 고양이 전체 데이터 조회
-app.get("/cats", (req: express.Request, res: express.Response) => {
-  try {
-    const cats = Cat;
-    // throw new Error("db connect error");
-    res.send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-//* READ 특정 고양이 데이터 조회
-app.get("/cats/:id", (req: express.Request, res: express.Response) => {
-  try {
-    const params = req.params;
-    const cats = Cat.find((cat) => {
-      return cat.id === params.id;
-    });
-    res.send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-//* CREATE 새로운 고양이 추가 api
-app.post("/cats", (req: express.Request, res: express.Response) => {
-  try {
-    const data = req.body;
-    console.log(data);
-    Cat.push(data); // create
-    res.status(200).send({
-      success: true,
-      data: { data },
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+app.use(catsRouter);
 
 // * 404 middlewart
 app.use((req, res, next) => {
